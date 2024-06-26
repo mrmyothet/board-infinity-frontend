cdea53f07e854c3f9ef553899188553d1cbea699
Myo Thet <mr.myothet@gmail.com>
2024-06-26 17:39:27 +0700

    Module 2 - javascript - conditional and loop

---
+++ Module1/Javascript/conditional.js
@@ -0,0 +1,22 @@
+let age = 18;
+
+if (age >= 18) {
+    console.log('You are an adult');
+}
+else {
+    console.log('You are not yet an adult');
+}
+
+let day = 2;
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+
+    default:
+        console.log('Invalid day');
+        break;
+}
