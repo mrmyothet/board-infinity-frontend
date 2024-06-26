08064f6d87bc24e985418de3ed0ee47d2512fdb1
Myo Thet <mr.myothet@gmail.com>
2024-06-26 12:16:31 +0700

    Module 2 - javascript - operators

---
+++ Module1/Javascript/operators.js
@@ -0,0 +1,36 @@
+let x = 10;
+let y = 5;
+
+let sum = x + y;
+console.log("Sum = " + sum);
+
+let difference = x - y;
+console.log("difference = " + difference);
+
+let product = x * y;
+console.log("product = " + product);
+
+let quotient = x / y;
+console.log("quotient = " + quotient);
+
+let remainder = x % y;
+console.log("remainder = " + remainder);
+
+let result = x ** y;
+console.log("result = " + result);
+
+//----------
+
+let a = 10;
+let b = 5;
+console.log(a == b);
+console.log(a != b);
+
+console.log(a === b);
+console.log(a !== b);
+
+console.log(a > b);
+console.log(a >= b);
+
+console.log(a < b);
+console.log(a <= b);
