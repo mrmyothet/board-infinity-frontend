a81278705355513c8eeabec277ec73446df9ecbf
Myo Thet <mr.myothet@gmail.com>
2024-06-26 17:15:16 +0700

    Module 2 - javascript - arrays

---
+++ Module1/Javascript/arrays.js
@@ -0,0 +1,17 @@
+let fruits = ['apples', 'oranges', 'bananas'];
+console.log(fruits[0]);
+
+fruits.push('grapes');
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+let numberArray = [1, 2, 3, 4, 5];
+console.log(numberArray);
+
+let stringArray = ["apple", "orange", "banana"];
+console.log(stringArray);
+
+let mixedArray = [1, "apple", true, { name: "John", age: 30 }];
+console.log(mixedArray);
