390949404d91c8ba80a07ab2ec9e0ad6f3464059
Myo Thet <mr.myothet@gmail.com>
2024-07-24 11:54:01 +0700

    Module 3 - React Hooks - useContext

--- Module3/react-app/src/App.jsx
+++ Module3/react-app/src/App.jsx
@@ -1,23 +1,71 @@
-import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import React, { useContext } from "react";
 import Greeting from "./components/Greeting";
 import Counter from "./components/Counter";
 import Increment from "./components/Increment";
 import UseEffectCounter from "./components/UseEffectCounter";
 import Posts from "./components/Posts";
 
+const ThemeContext = React.createContext("light");
+
+function Header() {
+  const theme = useContext(ThemeContext);
+
+  return (
+    <header
+      style={{
+        backgroundColor: theme === "light" ? "#fff" : "#000",
+        color: theme === "light" ? "#000" : "#fff",
+      }}
+    >
+      <h1>My Website Header</h1>
+    </header>
+  );
+}
+
+function Main() {
+  const theme = useContext(ThemeContext);
+
+  return (
+    <main
+      style={{
+        backgroundColor: theme === "light" ? "#fff" : "#000",
+        color: theme === "light" ? "#000" : "#fff",
+      }}
+    >
+      <h3>Main Content of the site</h3>
+    </main>
+  );
+}
+
+function Footer() {
+  const theme = useContext(ThemeContext);
+
+  return (
+    <footer
+      style={{
+        backgroundColor: theme === "light" ? "#fff" : "#000",
+        color: theme === "light" ? "#000" : "#fff",
+      }}
+    >
+      <h5>COPYRIGHT @ 2024 - SOLID PLM TECHNOLOGIES COMPANY LIMITED</h5>
+    </footer>
+  );
+}
+
 function App() {
   return (
     <div>
-      <Header />
-      <Footer />
-      <Greeting name="Alice" />
-      <Greeting name="John" />
-      <Counter />
-      <Increment />
-      <UseEffectCounter />
-      <Posts />
+      <ThemeContext.Provider value="dark">
+        <Header />
+        <Main />
+        <Footer />
+        <Greeting name="Alice" />
+        <Greeting name="John" />
+        <Counter />
+        <Increment />
+        <UseEffectCounter />
+        <Posts />
+      </ThemeContext.Provider>
     </div>
   );
 }
